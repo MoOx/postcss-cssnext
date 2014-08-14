c4d1c436e9eff9b1fb1a8418d8070fb403421135
Maxime Thirouin <m@moox.io>
2014-08-14 11:19:27 +0200

    Add standalone build for test

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var test = require("tape")
-
 var cssnext = require("..")
+var cssnextStandalone = require("../dist/cssnext.js")
 
 test("cssnext", function(t) {
   t.ok(typeof cssnext("html{}") === "string", "should return a string")
@@ -12,6 +12,21 @@ test("cssnext", function(t) {
  * Features tests
  */
 var toSlug = require("to-slug-case")
+var testFeature = function(t, feature, cssnext, version, source, input, expected) {
+  var options = {from: source, sourcemap: false, features: {}}
+
+  // disable all features
+  Object.keys(cssnext.features).forEach(function(key) { options.features[key] = false })
+
+  var css = cssnext(input, options)
+  t.notEqual(css, expected, version + ": should not add " + feature + " support if disabled")
+  t.equal(css, input, version + ": should not modify input if  " + feature + " is disabled")
+
+  // enable only the one we want to test
+  options.features[feature] = true
+  t.equal(cssnext(input, options), expected, version + ": should add " + feature + " support")
+}
+
 Object.keys(cssnext.features).forEach(function(name) {
   var slug = toSlug(name)
   var source = resolve("features/" + slug)
@@ -19,18 +34,15 @@ Object.keys(cssnext.features).forEach(function(name) {
   var expected = read("features/" + slug + ".expected")
 
   test(slug, function(t) {
-    var options = {from: source, sourcemap: false, features: {}}
+    testFeature(t, name, cssnext, "node.js", source, input, expected)
 
-    // disable all features
-    Object.keys(cssnext.features).forEach(function(key) { options.features[key] = false })
+    // we do not support @import in the browser
+    if ("import" === name) {
+      t.end()
+      return
+    }
 
-    var css = cssnext(input, options)
-    t.notEqual(css, expected, "should not add " + slug + " support if disabled")
-    t.equal(css, input,  "should not modify input if  " + slug + " is disabled")
-
-    // enable only the one we want to test
-    options.features[name] = true
-    t.equal(cssnext(input, options), expected, "should add " + slug + " support")
+    testFeature(t, name, cssnextStandalone, "browser", source, input, expected)
 
     t.end()
   })
@@ -39,7 +51,6 @@ Object.keys(cssnext.features).forEach(function(name) {
 /**
  * Sourcemap tests
  */
-
 test("sourcemap", function(t) {
   t.equal(
     cssnext(
@@ -75,3 +86,18 @@ function read(filename) {
   return require("fs").readFileSync(resolve(filename), "utf8")
 }
 
+/**
+ * Remove a fixture by `filename`.
+ *
+ * @param {String} filename
+ */
+
+// function remove(filename) {
+//   var fs = require("fs")
+//
+//   var file = resolve(filename)
+//   if (!fs.existsSync(file)) {
+//     return
+//   }
+//   fs.unlinkSync(file)
+// }
