8298d64e92948ef66605b70467122dbabf83ff00
Maxime Thirouin <m@moox.io>
2014-12-10 08:04:29 +0100

    windows: try to fix "'.' is not recognized" issue
    http://stackoverflow.com/questions/20765337/how-to-fix-is-not-an-interna
    l-or-external-command-error

--- test/cli.js
+++ test/cli.js
@@ -1,8 +1,9 @@
 /**
  * Test dependencies
  */
-var test = require("tape")
 var exec = require("child_process").exec
+
+var test = require("tape")
 
 var utils = require("./utils")
 var cssnext = require("..")
@@ -13,11 +14,12 @@ var cssnext = require("..")
 var input = utils.read("cli/input")
 var output = utils.read("cli/input.expected")
 
+var cssnextBin = "./\"bin/cssnext\""
 
 test("cli", function(t) {
   var planned = 0
 
-  exec("./bin/cssnext test/cli/input.css test/cli/output.css", function(err) {
+  exec(cssnextBin + " test/cli/input.css test/cli/output.css", function(err) {
     if (err) { throw err }
     var res = utils.read("cli/output")
     t.equal(res, output, "should read from a file and write to a file")
@@ -25,13 +27,13 @@ test("cli", function(t) {
   })
   planned+=1
 
-  exec("./bin/cssnext test/cli/input.css", function(err, stdout) {
+  exec(cssnextBin + " test/cli/input.css", function(err, stdout) {
     if (err) { throw err }
     t.equal(stdout, output, "should read from a file and write to stdout")
   })
   planned+=1
 
-  var childProcess = exec("./bin/cssnext", function(err, stdout) {
+  var childProcess = exec(cssnextBin + "", function(err, stdout) {
     if (err) { throw err }
     t.equal(stdout, output, "should read from stdin and write to stdout")
   })
@@ -39,33 +41,33 @@ test("cli", function(t) {
   childProcess.stdin.end()
   planned+=1
 
-  exec("./bin/cssnext test/cli/wtf.css", function(err, stdout, stderr) {
+  exec(cssnextBin + " test/cli/wtf.css", function(err, stdout, stderr) {
     t.ok(err && err.code === 1, "should return an error when input file is unreadable")
     t.ok(utils.contains(stderr, "Unable to read file"), "should show that the input file is not found")
   })
   planned+=2
 
-  exec("./bin/cssnext test/cli/error.css", function(err, stdout, stderr) {
+  exec(cssnextBin + " test/cli/error.css", function(err, stdout, stderr) {
     t.ok(err && err.code === 2, "should throw an error")
     t.ok(utils.contains(stderr, "encounters an error"), "should output a readable error")
     t.ok(utils.contains(stderr, "If this error looks like a bug, please report it here"), "should show the url where to report bugs")
   })
   planned+=3
 
-  exec("./bin/cssnext --verbose test/cli/input.css test/cli/output.css", function(err, stdout) {
+  exec(cssnextBin + " --verbose test/cli/input.css test/cli/output.css", function(err, stdout) {
     if (err) { throw err }
     t.ok(utils.contains(stdout, "Output written"), "should log on --verbose")
     utils.remove("cli/output")
   })
   planned+=1
 
-  exec("./bin/cssnext --compress test/compress/input.css", function(err, stdout) {
+  exec(cssnextBin + " --compress test/compress/input.css", function(err, stdout) {
     if (err) { throw err }
     t.equal(stdout, utils.read("compress/expected").trim(), "should compress on --compress")
   })
   planned+=1
 
-  exec("./bin/cssnext --sourcemap test/sourcemap/input.css", function(err, stdout) {
+  exec(cssnextBin + " --sourcemap test/sourcemap/input.css", function(err, stdout) {
     if (err) { throw err }
     t.equal(stdout, utils.read("sourcemap/expected-inline").trim(), "should add sourcemap on --sourcemap")
   })
@@ -78,20 +80,20 @@ test("cli", function(t) {
   features.forEach(function(feature) {
     var slug = toSlug(feature)
     var output = utils.read("features/" + slug)
-    exec("./bin/cssnext " + no + " test/features/" + slug + ".css", function(err, stdout) {
+    exec(cssnextBin + " " + no + " test/features/" + slug + ".css", function(err, stdout) {
       if (err) { throw err }
       t.equal(stdout, output, "should not modify input of '" + toSpace(feature) + "' fixture if all features are disabled")
     })
   })
   planned+=features.length
 
-  exec("./bin/cssnext --watch", function(err, stdout, stderr) {
+  exec(cssnextBin + " --watch", function(err, stdout, stderr) {
     t.ok(err && err.code === 3, "should return an error when <input> or <output> are missing when `--watch` option passed")
     t.ok(utils.contains(stderr, "--watch option need"), "should show an explanation when <input> or <output> are missing when `--watch` option passed")
   })
   planned+=2
 
-  exec("./bin/cssnext --watch test/cli/input.css", function(err, stdout, stderr) {
+  exec(cssnextBin + " --watch test/cli/input.css", function(err, stdout, stderr) {
     t.ok(err && err.code === 3, "should return an error when <output> is missing when `--watch` option passed")
     t.ok(utils.contains(stderr, "--watch option need"), "should show an explanation when <output> is missing when `--watch` option passed")
   })
