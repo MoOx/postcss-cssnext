d011ec602a8d2289315ccd5c660de737d7189169
Jason Kuhrt <jasonkuhrt@me.com>
2015-01-27 19:15:33 -0500

    First pass at integrating postcss-url into cssnext. There is one test that demonstrates url-writing in the face of @import.

--- test/features.js
+++ test/features.js
@@ -21,8 +21,14 @@ var testFeature = function(t, feature, cssnext, version, source, input, expected
   t.notEqual(css, expected, version + ": should not add " + feature + " support if disabled")
   t.equal(css, input, version + ": should not modify input if  " + feature + " is disabled")
 
-  // enable only the one we want to test
+  // enable only the one we want to test...
   options.features[feature] = true
+
+  // ...except "url" because we want to validate its behaviour when integrated
+  // with "import"
+  if (feature === "url") {
+    options.features["import"] = true
+  }
   t.equal(cssnext(input, options).trim(), expected.trim(), version + ": should add " + feature + " support")
 }
 
@@ -35,8 +41,8 @@ Object.keys(cssnext.features).forEach(function(name) {
   test(slug, function(t) {
     testFeature(t, name, cssnext, "node.js", source, input, expected)
 
-    // we do not support @import in the browser
-    if ("import" === name) {
+    // we do not support @import  or url rewriting in the browser
+    if ("import" === name || "url" === name) {
       t.end()
       return
     }
