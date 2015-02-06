5d179b34e8239eb0169be40754130b3f96aaa91b
Maxime Thirouin <m@moox.io>
2015-02-06 19:20:43 +0100

    Add stupid benchmarks per feature
    Ref #65
    
    First shot as today gives
    
    ```
    ❯ node benchmarks.js
    customProperties takes 0.20704s
    calc takes 0.03314s
    customMedia takes 0.01792s
    mediaQueriesRange takes 0.00871s
    customSelectors takes 0.00889s
    colorRebeccapurple takes 0.07094s
    colorHwb takes 0.07437s
    colorGray takes 0.09796s
    colorHexAlpha takes 0.07138s
    colorFunction takes 0.11102s
    fontVariant takes 0.00786s
    filter takes 0.08778s
    rem takes 0.01469s
    autoprefixer takes 1.04424s
    ``

---
+++ benchmarks.js
@@ -0,0 +1,23 @@
+var cssnext = require("./")
+var t = require("microtime")
+var assign = require("object-assign")
+
+var input = require("fs").readFileSync("./test/fixtures/cases/example.css")
+
+// test each features
+var keys = Object.keys(cssnext.features)
+var allOff = {}
+
+keys.forEach(function(k) {
+  allOff[k] = false
+})
+
+keys.forEach(function(k) {
+  var enable = assign({}, allOff)
+  enable[k] = true
+  var start = t.now()
+  cssnext(input, {features: enable})
+  var stop = t.now()
+
+  console.log(k + " takes " + ((stop - start) / 100000) + "s") //, enable)
+})
