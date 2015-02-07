57d5db369547da9fce885d476e2b6d8a6f32bc4f
Maxime Thirouin <m@moox.io>
2015-02-07 07:30:31 +0100

    Make relevant benchmarks
    [skip ci]

--- benchmarks.js
+++ benchmarks.js
@@ -2,7 +2,14 @@ var cssnext = require("./")
 var t = require("microtime")
 var assign = require("object-assign")
 
-var input = require("fs").readFileSync("./test/fixtures/cases/example.css")
+var input = require("fs").readFileSync("./test/fixtures/benchmarks.css", {encoding: "utf8"})
+
+// make a 2MB input
+for(var i=0;i<=10;i++) {
+  input += input
+}
+// check the real input
+// require("fs").writeFileSync("./test/fixtures/bench.css", input)
 
 // test each features
 var keys = Object.keys(cssnext.features)
@@ -12,11 +19,13 @@ keys.forEach(function(k) {
   allOff[k] = false
 })
 
+console.log("Each features is tested on a 2MB input (>100 000 lines)\n")
+
 keys.forEach(function(k) {
   var enable = assign({}, allOff)
   enable[k] = true
   var start = t.now()
-  cssnext(input, {features: enable})
+  var out = cssnext(input, {features: enable})
   var stop = t.now()
 
   console.log(k + " takes " + ((stop - start) / 1000000) + "s") // , enable)
