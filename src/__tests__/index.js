90d38ff66d465be089850e774068ce12d45e1465
Yordis Prieto <yordis.prieto@gmail.com>
2016-08-16 00:51:24 -0400

    Internal: add regression testing (#298)

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,11 +1,28 @@
 import tape from "tape"
 
+import utils from "./utils"
 import postcssnext from ".."
 
 tape("postcss-cssnext is a postcss plugin", (t) => {
   t.ok(
     typeof postcssnext.process === "function",
     "should have the postcss process() function available"
+  )
+
+  t.end()
+})
+
+tape("cssnext regression test", (t) => {
+  const input = utils.readFixture("regression")
+  const expected = utils.readFixture("regression.expected")
+  const actual = postcssnext().process(input).css.trim()
+
+  utils.write(utils.fixturePath("regression.actual"), actual)
+
+  t.equal(
+    actual,
+    expected.trim(),
+    "should pass the regression"
   )
 
   t.end()
