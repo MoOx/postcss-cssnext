b0db778a422abb6524f729740f940e3fa10a8ef6
Jonathan Neal <jonathantneal@hotmail.com>
2017-05-17 16:00:47 -0400

    Use PostCSS 6
    - Updates tests for whitespacing, which PostCSS 6 captures more
    accurately.

--- src/__tests__/option.browsers.js
+++ src/__tests__/option.browsers.js
@@ -5,7 +5,7 @@ import cssnext from ".."
 tape("cssnext browsers option", function(t) {
 
   const customPropsInput = ":root{--foo:bar}baz{qux:var(--foo)}"
-  const customPropsOutput = "baz{qux: bar}"
+  const customPropsOutput = "baz{qux:bar}"
 
   // fx 30 doesn't handle custom prop
   t.equal(
